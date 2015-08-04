f9860fbf833ed1bf6ad4145f8497ed5813021fe7
Luis Echegaray <mrluisechegaray@gmail.com>
2015-08-04 09:45:30 -0400

    added Odata functionality

--- ALM.Client/app/products/productListCtrl.js
+++ ALM.Client/app/products/productListCtrl.js
@@ -11,8 +11,11 @@
 
         vm.searchCriteria = "GDN";
 
-        productResource.query({search: vm.searchCriteria}, function (data) {
-            vm.products = data;
-        });
+        productResource.query({
+            $filter: "contains(ProductCode, 'GDN') and Price le 20",
+            $orderby: "Price desc"},
+            function (data) {
+                vm.products = data;
+            });
     }
 }());
